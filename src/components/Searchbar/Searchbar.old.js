38830f4282dfef8a9827af623da0f9c0e9c73030
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-08-28 15:38:02 +0300

    add

---
+++ src/components/Searchbar/Searchbar.old.js
@@ -0,0 +1,40 @@
+import { Component } from "react";
+import css from "./Searchbar.module.css"
+
+export class Searchbar extends Component {
+  state = {
+    query: '',
+  };
+
+  handleChange = ({ target: {value}}) => {
+    this.setState({ query: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.query);
+  };
+
+  render() {
+    return (
+      <header className={css.searchbar}>
+        <form onSubmit={this.handleSubmit} className={css.searchForm}>
+          <button type="submit" className={css.button}>
+            <span className={css.buttonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.input}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.query}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
